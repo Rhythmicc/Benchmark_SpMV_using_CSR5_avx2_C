087f93be6c6a1d4732fcc2d84ac1195b5e3f5709
Rhythmicc <RhythmLian@outlook.com>
2023-02-24 19:03:46 +0800

    update

---
+++ dist/CMakeFiles/C_CSR5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_CSR5.dir/main.c.o"
+  "CMakeFiles/C_CSR5.dir/main.c.o.d"
+  "C_CSR5"
+  "C_CSR5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_CSR5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ dist/CMakeFiles/C_CSR5.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for C_CSR5.
+# This may be replaced when dependencies are built.
